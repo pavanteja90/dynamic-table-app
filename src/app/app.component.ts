2c0e3eb15e7988209ebd65eb2a05cf4c444ae2d9
Pavan <pavanteja.bhatta@gmail.com>
2018-03-10 11:58:14 +0530

    Included popper.js for Bootstrap Dropdown to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  private id: string = "#a1";
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    $("#a1").addClass("current-tab");
+  }
+
+  navigate(id){
+    $(this.id).removeClass("current-tab");
+    this.id = "#a"+id.toString();
+    $(this.id).addClass("current-tab");
+  }
 }
